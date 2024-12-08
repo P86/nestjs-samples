8b41a446cbdd3f48bf24f3fe27ffb3c53b1bcfd1
Arkadiusz Piznal <apiznal@gmail.com>
2024-12-08 16:22:27 +0100

    test

--- http/src/app.module.ts
+++ http/src/app.module.ts
@@ -2,10 +2,10 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+// test
 @Module({
   imports: [HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
